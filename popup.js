54f67cea464a8264a1ff3f7ba24659d8408a3d80
elad <eladc5492@gmail.com>
2019-06-12 17:45:46 +0300

    invite popup

---
+++ popup.js
@@ -0,0 +1,25 @@
+var logged = false;
+    var firstTimeInPage = true;
+    $(document).ready(function(){
+
+        //setTimeout(function(){ Pop();},1000);
+        Pop();
+        // When the user clicks on <span> (x), close the modal
+         $("#close").click(function(){
+            $("#myModal").css("display", "none");
+        });
+        
+        // When the user clicks anywhere outside of the modal, close it
+        $.onclick = function(event) {
+          if (event.target == modal) {
+            $$("#myModal").css("display", "none");
+          }
+        }
+    });
+    function Pop()
+    {
+        if(logged == true) return;
+        if(firstTimeInPage == false) return;
+        firstTimeInPage = false;
+        document.getElementById("myModal").style.display = "block";
+    }
