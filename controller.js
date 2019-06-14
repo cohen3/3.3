2a0ab82771ce269b860e6851c49dd2df934501e7
elad <eladc5492@gmail.com>
2019-06-14 15:00:50 +0300

    push

--- controller.js
+++ controller.js
@@ -23,13 +23,48 @@ app.controller('myCtrl', function($scope, $http) {
     var slideIndex = 0;
     var values = [];
     showSlides();
-    $scope.foo1 = function() {
-        return $scope.firstName + " " + $scope.lastName;
-    };
-    $scope.foo2 = function() {
-        return $scope.lastName + " " + $scope.firstName;
-    };
-       $scope.getName=function(){
+    function getRecommended()
+    {
+        //cat = _.sample(cat, 2)
+        $http({
+            method: 'GET',
+            url: 'http://localhost:3000/rand3POI/0.2',
+          }).then(function(response) {
+              $scope.resp = response.data[0]['picture_link'];
+              values = response.data;
+            }).catch(function(error){
+                alert("here"+error.status);
+            });
+    }
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if(values.length > 0){
+            for (i = 0; i < slides.length; i++){
+                slides[i].style.display = "none";
+            }
+            document.getElementById('txt1').innerText = values[0]['name'];
+            document.getElementById('txt2').innerText = values[1]['name'];
+            document.getElementById('txt3').innerText = values[2]['name'];
+
+            document.getElementById('im1').src = values[0]['picture_link'];
+            document.getElementById('im2').src = values[1]['picture_link'];
+            document.getElementById('im3').src = values[2]['picture_link'];
+            // $scope.txt2 = values[1]['name'];
+            // $scope.txt3 = values[2]['name'];
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+        }
+        setTimeout(showSlides, 4000); // Change image every 2 seconds
+    }
+    $scope.getName=function(){
         return  $scope.firstName+ $scope.lastName; };
         $scope.validuser = function() {
             $http.get('http://localhost:3000/login/' + $scope.tmpname + '/' + $scope.password).then(function (res) {
@@ -64,47 +99,6 @@ app.controller('myCtrl', function($scope, $http) {
             // or server returns response with an error status.
           });
     }
-
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if(values.length > 0){
-            for (i = 0; i < slides.length; i++){
-                slides[i].style.display = "none";
-            }
-            document.getElementById('txt1').innerText = values[0]['name'];
-            document.getElementById('txt2').innerText = values[1]['name'];
-            document.getElementById('txt3').innerText = values[2]['name'];
-
-            document.getElementById('im1').src = values[0]['picture_link'];
-            document.getElementById('im2').src = values[1]['picture_link'];
-            document.getElementById('im3').src = values[2]['picture_link'];
-            // $scope.txt2 = values[1]['name'];
-            // $scope.txt3 = values[2]['name'];
-            slideIndex++;
-            if (slideIndex > slides.length) {slideIndex = 1}
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += " active";
-        }
-        setTimeout(showSlides, 4000); // Change image every 2 seconds
-    }
-    function getRecommended()
-    {
-        //cat = _.sample(cat, 2)
-        $http({
-            method: 'GET',
-            url: 'http://localhost:3000/rand3POI/0.2',
-        }).then(function(response) {
-            $scope.resp = response.data[0]['picture_link'];
-            values = response.data;
-        }).catch(function(error){
-            alert(error.status);
-        });
-    }
 });
 
 angular.element(document).ready(function () {
@@ -112,3 +106,4 @@ angular.element(document).ready(function () {
     // var a = document.getElementById("test");
     // a.innerText = "loaded";
     });
+
